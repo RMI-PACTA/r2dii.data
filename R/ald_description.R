fa29bd2baad58e36eef5904c1356c1de30882d48
Mauro Lepore <maurolepore@gmail.com>
2020-03-06 18:06:25 -0600

    Copy _description

---
+++ R/ald_description.R
@@ -0,0 +1,15 @@
+#' Description of a generic ald dataset
+#'
+#' Asset Level Data: Physical asset production and capacity data across a
+#' variety of sectors and technologies, including forward looking projected
+#' additions or retirements.
+#'
+#' @name ald_description
+#' @seealso [data_dictionary], [ald_demo].
+#' @family generic datasets
+#' @examples
+#' library(dplyr)
+#'
+#' data_dictionary() %>%
+#'   filter(dataset == "ald")
+NULL
---
+++ R/loanbook_description.R
@@ -0,0 +1,16 @@
+#' Description of a generic loanbook dataset
+#'
+#' Corporate credit portfolio of banks that holds information of all loans given
+#' by a bank including information on the borrower, the sector classification of
+#' the borrower and the size of the loan. The information comes from banks and
+#' is proprietary data.
+#'
+#' @name loanbook_description
+#' @seealso [data_dictionary]
+#' @family generic datasets
+#' @examples
+#' library(dplyr)
+#'
+#' data_dictionary() %>%
+#'   filter(dataset == "loanbook")
+NULL
