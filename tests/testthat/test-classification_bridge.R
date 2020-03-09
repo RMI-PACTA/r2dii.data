24a29a35b9ca263eb713bc5bb87394e3c1fae212
Mauro Lepore <maurolepore@gmail.com>
2020-03-09 15:30:39 -0500

    Simpler test removes dependency on r2dii.utils

--- tests/testthat/test-classification_bridge.R
+++ tests/testthat/test-classification_bridge.R
@@ -1,20 +1,13 @@
-test_that("all classification data has minimim expected names", {
-  ends_with_classification <- grep(
-    pattern = "_classification$",
-    x = exported_data("r2dii.data"),
-    value = TRUE
-  )
-
-  classification_list <- ends_with_classification %>%
-    purrr::map(~ get(.x, envir = as.environment("package:r2dii.data"))) %>%
-    purrr::set_names(ends_with_classification)
-
-  # https://github.com/2DegreesInvesting/r2dii.match/issues/7
+test_that("all classification datasets have minimum expected names", {
+  all_data <- enlist_datasets("r2dii.data", pattern = "")
+  classification_data <- all_data[grepl("_classification", names(all_data))]
   crucial <- c("code", "sector", "borderline")
-  expect_error(
-    purrr::walk(
-      classification_list, ~ r2dii.utils::check_crucial_names(.x, crucial)
-    ),
-    NA
+
+  actual <- purrr::map_lgl(classification_data, ~ all(crucial %in% names(.x)))
+  expect <- c(
+    isic_classification = TRUE,
+    nace_classification = TRUE,
+    naics_classification = TRUE
   )
+  expect_equal(actual, expect)
 })
