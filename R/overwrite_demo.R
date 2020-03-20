1deae805a3ac2b93fb5a189393e4f1a4c6ffc253
Mauro Lepore <maurolepore@gmail.com>
2020-03-20 13:02:59 -0500

    Document format for overwrite_demo

--- R/overwrite_demo.R
+++ R/overwrite_demo.R
@@ -5,6 +5,16 @@
 #' @family demo datasets
 #' @seealso [data_dictionary]
 #'
+#' @format
+#' `overwrite_demo` is a [data.frame] with columns:
+#' * `id_2dii` (character): IDs of the entities to overwrite.
+#' * `level` (character): Which level should be overwritten (e.g.
+#' direct_loantaker or ultimate_parent).
+#' * `name` (character): Overwrite name (if only overwriting sector, type NA).
+#' * `sector` (character): Overwrite sector (if only overwriting name, type NA).
+#' * `source` (character): What is the source of this information (leave as
+#' "manual" for now, may remove this flag later).
+#'
 #' @examples
 #' head(overwrite_demo)
 "overwrite_demo"
