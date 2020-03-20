6702d07bb4670bd8a98d1332af5efa821a2d02c0
Mauro Lepore <maurolepore@gmail.com>
2020-03-20 12:47:20 -0500

    Document format for

--- R/classification_bridge.R
+++ R/classification_bridge.R
@@ -8,9 +8,8 @@
 #'
 #' @family datasets for bridging sector classification codes
 #'
-#' @return A [data.frame] (subclass 'tbl').
-#'
 #' @name classification_bridge
+#' @aliases isic_classification nace_classification naics_classification
 #'
 #' @examples
 #' head(isic_classification)
@@ -21,10 +20,36 @@
 NULL
 
 #' @rdname classification_bridge
+#' @format
+#' `classification_bridge` is a [data.frame] with columns:
+#' * `borderline` (logical): Flag indicating if 2dii sector and classification
+#' code are a borderline match.
+#' * `code` (character): Original ISIC code.
+#' * `code_level` (double): Level of granularity of ISIC code.
+#' * `sector` (character): Associated 2dii sector.
+#'
 "isic_classification"
 
 #' @rdname classification_bridge
+#' @format
+#' `nace_classification` is a data.frame with columns:
+#' * `borderline` (logical): Flag indicating if 2dii sector and classification
+#' code are a borderline match.
+#' * `code` (double): Formatted NACE code removing periods.
+#' * `code_level` (double): Level of granularity of NACE code.
+#' * `original_code` (double): Original NACE code.
+#' * `sector` (character): Associated 2dii sector.
+#'
 "nace_classification"
 
 #' @rdname classification_bridge
+#' @format
+#' `naics_classification` is a data.frame with columns:
+#' * `borderline` (logical): Flag indicating if 2dii sector and classification
+#' code are a borderline match.
+#' * `code` (double): Formatted NAICS code.
+#' * `code_level` (double): Level of granularity of NAICS code.
+#' * `original_code` (double): Original NAICS code.
+#' * `sector` (character): Associated 2dii sector.
+#'
 "naics_classification"
