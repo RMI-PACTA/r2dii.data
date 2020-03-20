034180585f1ad3a9ea4a8dbb8d16dee4fe34a573
Mauro Lepore <maurolepore@gmail.com>
2020-03-20 12:56:27 -0500

    Document format for

--- R/data_dictionary.R
+++ R/data_dictionary.R
@@ -1,7 +1,13 @@
 #' A dataset that defines the columns of r2dii datasets
 #'
 #' @family demo datasets
-#' @return A [data.frame] (subclass 'tbl').
+#' @format
+#' `data_dictionary` is a [data.frame] with columns:
+#' * `column` (character): The name of a dataset-column.
+#' * `dataset` (character): The name of a dataset.
+#' * `definition` (character): The definition of a dataset-column.
+#' * `typeof` (character): The result of `typeof()`, one of double, integer,
+#' logical, or character.
 #'
 #' @examples
 #' head(data_dictionary)
