7e6dc49723cdb9879e9da37f03e128717799ef42
Mauro Lepore <maurolepore@gmail.com>
2020-03-20 13:05:38 -0500

    Document format for sector_classifications

--- R/sector_classifications.R
+++ R/sector_classifications.R
@@ -3,9 +3,13 @@
 #' @seealso [data_dictionary].
 #' @family datasets for bridging sector classification codes
 #'
-#' @return A [data.frame] (subclass 'tbl'). The column `code_system` names one
-#'   of the classification systems that 2dii uses. All other columns are defined
-#'   at [data_dictionary].
+#' @format
+#' `sector_classifications` is a [data.frame] with columns:
+#' * `borderline` (character): Flag indicating if 2dii sector and classification
+#' code are a borderline match.
+#' * `code` (character): Formatted code.
+#' * `code_system` (character): Code system.
+#' * `sector` (character): Associated 2dii sector.
 #'
 #' @examples
 #' head(sector_classifications)
