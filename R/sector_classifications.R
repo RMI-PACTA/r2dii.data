c43892df5e3fd9925190a135ade93c286399f711
Mauro Lepore <maurolepore@gmail.com>
2020-03-21 18:10:29 -0500

    Polish all the way to last dataset

--- R/sector_classifications.R
+++ R/sector_classifications.R
@@ -1,8 +1,8 @@
 #' A view of available sector classification datasets
 #'
 #' @description
-#' This dataset lists all sector classification code standards (e.g. NACE, ISIC)
-#' currently bridged.
+#' This dataset lists all sector classification code standards used by 'PACTA'
+#' (<https://2degrees-investing.org/resource/pacta/>).
 #' @template info_classification-datasets
 #'
 #' @family datasets for bridging sector classification codes
