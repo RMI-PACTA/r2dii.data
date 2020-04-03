49f80814dc86488454c34ce6560eceb53d1dff7b
Mauro Lepore <maurolepore@gmail.com>
2020-04-03 06:43:55 -0500

    Minor rewording and styling

--- R/scenario_demo_2020.R
+++ R/scenario_demo_2020.R
@@ -3,9 +3,9 @@
 #' @description
 #' Fake data representing a prepared climate scenario dataset, to be used
 #' directly in a PACTA analysis. It imitates data from climate scenario
-#' providers such as the International Energy Agency (IEA). Climate scenarios
-#' are prepared internally by 2Dii for analysis by determining growth rates in
-#' comparison to a start year.
+#' providers, such as the International Energy Agency (IEA), that
+#' 2DII (<https://2degrees-investing.org/>) prepares for analysis by
+#' determining growth rates in comparison to a start year.
 #' @template info_demo-datasets
 #'
 #' @family demo datasets
