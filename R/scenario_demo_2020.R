20af53ae4f47a74ca03035b1801ff6954875aeb9
Mauro Lepore <maurolepore@gmail.com>
2020-04-03 06:33:20 -0500

    Update scenario_demo_2020.R

--- R/scenario_demo_2020.R
+++ R/scenario_demo_2020.R
@@ -1,11 +1,11 @@
 #' A prepared climate scenario dataset for demonstration
+#'
 #' @description
 #' Fake data representing a prepared climate scenario dataset, to be used
 #' directly in a PACTA analysis. It imitates data from climate scenario
 #' providers such as the International Energy Agency (IEA). Climate scenarios
 #' are prepared internally by 2Dii for analysis by determining growth rates in
 #' comparison to a start year.
-#'
 #' @template info_demo-datasets
 #'
 #' @family demo datasets
