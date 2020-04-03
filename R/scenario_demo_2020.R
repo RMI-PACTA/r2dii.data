3757f99f950f10f6d2cfbf6dcaac81a5038816f4
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-04-03 12:07:47 +0200

    document scenario_demo_2020

---
+++ R/scenario_demo_2020.R
@@ -0,0 +1,15 @@
+#' A prepared climate scenario dataset for demonstration
+#' @description
+#' Fake data representing a prepared climate scenario dataset, to be used
+#' directly in a PACTA analysis. It imitates data from climate scenario
+#' providers such as the International Energy Agency (IEA). Climate scenarios
+#' are prepared internally by 2Dii for analysis by determining growth rates in
+#' comparison to a start year.
+#'
+#' @template info_demo-datasets
+#'
+#' @family demo datasets
+#' @seealso [data_dictionary]
+#' @examples
+#' head(scenario_demo_2020)
+"scenario_demo_2020"
