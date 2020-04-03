49a5c06e383ee504dcb283f13325cba2e8a95134
Mauro Lepore <maurolepore@gmail.com>
2020-04-03 16:58:26 -0500

    Revert "Copy add_market_share_columns from r2dii.scenario"
    This reverts commit 0d9a1fc8f55838d9c68f203c18eb7a58e1aecc4b.
    That commit was only added to make it possible to recover the
    source code of add_market_share_columns in case that function
    becomes unavailable from r2dii.scenario.

--- data-raw/scenario_demo.R
+++ data-raw/scenario_demo.R
@@ -1,5 +1,3 @@
-source(here::here("data-raw", "add_market_share_columns.R"))
-
 library(magrittr)
 
 # Accessed on 2020-03-12, source r2dii.dataraw::scenario_demo
@@ -7,7 +5,8 @@ library(magrittr)
 path <- here::here("data-raw/scenario_demo.csv")
 
 scenario_demo_2020 <- readr::read_csv(path) %>%
-  add_market_share_columns(start_year = 2020) %>%
+  # remotes::install_github("2degreesinvesting/r2dii.scenario@0.0.0.9002")
+  r2dii.scenario::add_market_share_columns(start_year = 2020) %>%
   dplyr::select(-c("value", "units"))
 
 usethis::use_data(scenario_demo_2020, overwrite = TRUE)
