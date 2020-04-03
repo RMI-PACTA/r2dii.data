cffd6d08b8b2388f86b4f84187d87734e12d8175
Mauro Lepore <maurolepore@gmail.com>
2020-04-03 06:54:11 -0500

    Force to update reference output manually

--- tests/testthat/test-scenario_demo.R
+++ tests/testthat/test-scenario_demo.R
@@ -1,6 +1,10 @@
 test_that("hasn't changed", {
-  expect_known_value("scenario_demo_2020", "ref-scenario_demo_2020")
+  expect_known_value(
+    "scenario_demo_2020", "ref-scenario_demo_2020",
+    update = FALSE
+  )
   expect_known_output(
-    scenario_demo_2020, "ref-scenario_demo_2020-output.txt", print = TRUE
+    scenario_demo_2020, "ref-scenario_demo_2020-output.txt",
+    print = TRUE
   )
 })
