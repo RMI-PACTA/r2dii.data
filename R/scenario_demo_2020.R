1eafa31bf6159a7795f525b9a9ba50b188c05156
Mauro Lepore <maurolepore@gmail.com>
2020-04-03 07:10:50 -0500

    More rewordging

--- R/scenario_demo_2020.R
+++ R/scenario_demo_2020.R
@@ -1,11 +1,11 @@
 #' A prepared climate scenario dataset for demonstration
 #'
 #' @description
-#' Fake data representing a prepared climate scenario dataset, to be used
-#' directly in a PACTA analysis. It imitates data from climate scenario
-#' providers, such as the International Energy Agency (IEA), that
-#' 2DII (<https://2degrees-investing.org/>) prepares for analysis by
-#' determining growth rates in comparison to a start year.
+#' Fake climate scenario dataset, prepared for the software PACTA (Paris
+#' Agreement Capital Transition Assessment). It imitates climate scenario
+#' provided by organizations such as the International Energy Agency (IEA) and
+#' prepared by 2DII (<https://2degrees-investing.org/>) by determining growth
+#' rates in comparison to a start year.
 #' @template info_demo-datasets
 #'
 #' @family demo datasets
