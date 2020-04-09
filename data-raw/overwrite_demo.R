4807e9a46bcc434ed52a0710ee00043034c6a929
Mauro Lepore <maurolepore@gmail.com>
2020-04-09 07:58:13 -0500

    Simpler overwrite_demo

--- data-raw/overwrite_demo.R
+++ data-raw/overwrite_demo.R
@@ -1,5 +1,7 @@
+source(here::here("data-raw/utils.R"))
+
 # Source: @jdhoffa
 path <- here::here("data-raw/overwrite_demo.csv")
-overwrite_demo <- readr::read_csv(path)
+overwrite_demo <- remove_spec(readr::read_csv(path))
 
 usethis::use_data(overwrite_demo, overwrite = TRUE)
