d1d7a2d3d6d15963e50eeb2d1110a1d4e6cbc8a6
Mauro Lepore <maurolepore@gmail.com>
2020-04-14 13:58:34 -0500

    Remove needless code to create loanbook_demo (#46)
    If it has a new column tests will fail, so no need to check here.

--- data-raw/loanbook_demo.R
+++ data-raw/loanbook_demo.R
@@ -1,9 +1,8 @@
+source(here::here("data-raw/utils.R"))
+
 # Source: https://github.com/2DegreesInvesting/r2dii.dataraw/pull/4
 
 path <- here::here("data-raw/loanbook_demo.csv")
-# The column `X1` may exist and contain needless rownames -- that's likely
-# because the data was read with `read.table()` using `row.names = FALSE`
-if (hasName(data, "X1")) stop("Must remove column `X1`")
-loanbook_demo <- remove_rownames_column(readr::read_csv(path))
+loanbook_demo <- readr::read_csv(path)
 
 usethis::use_data(loanbook_demo, overwrite = TRUE)
