74542a5162882e21048965fb684a345a469691c4
Mauro Lepore <maurolepore@gmail.com>
2020-04-15 16:51:56 -0500

    Assign 'company_name' to column id_name

--- data-raw/ald_scenario_demo.R
+++ data-raw/ald_scenario_demo.R
@@ -25,7 +25,7 @@ new_names_from_old_names <- c(
 #> [1] '0.8.99.9002'
 ald_scenario_demo <- ald_demo %>%
   dplyr::rename(dplyr::all_of(new_names_from_old_names)) %>%
-  dplyr::mutate(id_name = "FIXME_id_name", .before = 1L) %>%
+  dplyr::mutate(id_name = "company_name", .before = 1L) %>%
   dplyr::mutate(
     ald_emission_factor_unit = "FIXME_ald_emission_factor_unit",
     .after = ald_emission_factor
