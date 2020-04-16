6114be450178eaf29a96597ad5d20cc5f6d4c42f
Mauro Lepore <maurolepore@gmail.com>
2020-04-16 15:58:37 -0500

    Write doc source

---
+++ R/ald_scenario_demo.R
@@ -0,0 +1,19 @@
+#' FIXME Asset level data connected to the scenario data
+#'
+#' @description
+#' FIXME This dataset is the starting point of any PACTA analysis, including the
+#' bank's methodology. It can be used for matching as well as for the analysis
+#' itself. The dataset includes the asset level data connected to the scenario
+#' data.
+#'
+#' @template info_demo-datasets
+#'
+#' @family demo datasets
+#' @seealso [data_dictionary]
+#'
+#' @format
+#' FIXME
+#'
+#' @examples
+#' head(ald_scenario_demo)
+"ald_scenario_demo"
