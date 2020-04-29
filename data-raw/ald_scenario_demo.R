db8b146d47930e32096e4b7040caf1d80e2403ff
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-04-29 11:41:04 +0200

    fix inner_join to include pick_plant_location
    the joined file should already filter out mis-matched regions

--- data-raw/ald_scenario_demo.R
+++ data-raw/ald_scenario_demo.R
@@ -1,30 +1,48 @@
 source(here::here("data-raw/utils.R"))
 
+# Functions ---------------------------------------------------------------
+new_names_from_old_names <- function(){
+  c(
+    "id" = "name_company",
+    "ald_sector" = "sector",
+    "technology" = "technology",
+    "ald_production_unit" = "production_unit",
+    "year" = "year",
+    "ald_production" = "production",
+    "ald_emission_factor" = "emission_factor",
+    "country_of_domicile" = "country_of_domicile",
+    "ald_location" = "plant_location",
+    "FIXME_number_of_assets" = "number_of_assets",
+    "is_ultimate_owner" = "is_ultimate_owner",
+    "FIXME_is_ultimate_listed_owner" = "is_ultimate_listed_owner",
+    "FIXME_ald_timestamp" = "ald_timestamp"
+  )
+}
+
+scenario_columns <- function() {
+  c(
+    sector_ald = "sector",
+    technology = "technology",
+    year = "year"
+  )
+}
+
+pick_plant_location_in_region <- function(data) {
+  data %>% dplyr::mutate(plant_location = tolower(.data$plant_location)) %>%
+    dplyr::inner_join(r2dii.data::region_isos,
+                      by = c("region", "plant_location" = "isos")
+    )
+}
+
 # Rename columns of ald_demo as per @2diiKlaus's comment ------------------
 # https://github.com/2DegreesInvesting/r2dii.data/pull/24#issuecomment-606408655
 path <- here::here("data-raw/ald_demo.csv")
 ald_demo <- remove_spec(readr::read_csv(path))
 
-new_names_from_old_names <- c(
-  "id" = "name_company",
-  "ald_sector" = "sector",
-  "technology" = "technology",
-  "ald_production_unit" = "production_unit",
-  "year" = "year",
-  "ald_production" = "production",
-  "ald_emission_factor" = "emission_factor",
-  "country_of_domicile" = "country_of_domicile",
-  "ald_location" = "plant_location",
-  "FIXME_number_of_assets" = "number_of_assets",
-  "is_ultimate_owner" = "is_ultimate_owner",
-  "FIXME_is_ultimate_listed_owner" = "is_ultimate_listed_owner",
-  "FIXME_ald_timestamp" = "ald_timestamp"
-)
-
 # packageVersion("dplyr")
 # > [1] '0.8.99.9002'
 ald_scenario_demo <- ald_demo %>%
-  dplyr::rename(dplyr::all_of(new_names_from_old_names)) %>%
+  dplyr::rename(dplyr::all_of(new_names_from_old_names())) %>%
   dplyr::mutate(id_name = "company_name", .before = 1L) %>%
   dplyr::mutate(
     ald_emission_factor_unit = glue::glue("{ald_sector} emission_factor"),
@@ -33,8 +51,6 @@ ald_scenario_demo <- ald_demo %>%
   dplyr::inner_join(r2dii.data::scenario_demo_2020)
 
 use_data(ald_scenario_demo, overwrite = TRUE)
-
-
 
 # Data dictionary ---------------------------------------------------------
 
