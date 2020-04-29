c68c896cd4f3a2c8057304ddd6b1649b542c1b0f
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-04-29 11:18:16 +0200

    add description to ald_scenario_demo

--- R/ald_scenario_demo.R
+++ R/ald_scenario_demo.R
@@ -1,10 +1,9 @@
-#' FIXME Asset level data connected to the scenario data
+#' Asset level data connected to prepared scenario data for demonstration
 #'
-#' @description
-#' FIXME This dataset is the starting point of any PACTA analysis, including the
-#' bank's methodology. It can be used for matching as well as for the analysis
-#' itself. The dataset includes the asset level data connected to the scenario
-#' data.
+#' @description This dataset is the starting point of any PACTA analysis,
+#' including the bank's methodology. It can be used for matching as well as for
+#' the analysis itself. The dataset includes the asset level data connected to
+#' the prepared scenario data.
 #'
 #' @template info_demo-datasets
 #'
