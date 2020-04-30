ea6440acdfc93a7c50c31899c89bbdf2d8800be6
Mauro Lepore <maurolepore@gmail.com>
2020-04-29 21:57:03 -0500

    Enlarge regression reference

--- tests/testthat/test-ald_scenario_demo.R
+++ tests/testthat/test-ald_scenario_demo.R
@@ -1,7 +1,6 @@
 test_that("hasn't changed", {
-  # FIXME: Store the entire object, not just the head
   expect_known_value(
-    head(as.data.frame(ald_scenario_demo), 20),
+    ald_scenario_demo,
     "ref-ald_scenario_demo",
     update = FALSE
   )
