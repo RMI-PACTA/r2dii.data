6a9fb10a0408a5cba13672d61e1d94f323d6071e
Mauro Lepore <maurolepore@gmail.com>
2020-05-14 13:49:31 -0500

    Document region_isos_demo

---
+++ R/region_isos_demo.R
@@ -0,0 +1,15 @@
+#' @inherit region_isos title
+#'
+#' @description
+#' This dataset maps codes representing countries to regions. It is similar to
+#' but smaller than [region_isos].
+#'
+#' @template info_demo-datasets
+#' @template info_iso-codes
+#'
+#' @family iso codes
+#' @family demo datasets
+#'
+#' @examples
+#'region_isos_demo
+"region_isos_demo"
