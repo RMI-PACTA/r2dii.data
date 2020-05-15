29d95cef4736fc068284913d2932238be91d766d
Mauro Lepore <maurolepore@gmail.com>
2020-05-15 12:40:40 -0500

    Remove volatile regression test for output

--- tests/testthat/test-ald_scenario_demo.R
+++ tests/testthat/test-ald_scenario_demo.R
@@ -4,11 +4,3 @@ test_that("hasn't changed", {
     update = FALSE
   )
 })
-
-test_that("hasn't changed", {
-  expect_known_output(
-    ald_scenario_demo, "ref-ald_scenario_demo-output",
-    print = TRUE,
-    update = FALSE
-  )
-})
