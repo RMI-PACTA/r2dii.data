495484d856499fb6e1cb1e3407f1ee1421320e65
Mauro Lepore <maurolepore@gmail.com>
2020-05-15 10:51:35 -0500

    Style for consistency

--- tests/testthat/test-ald_scenario_demo.R
+++ tests/testthat/test-ald_scenario_demo.R
@@ -1,15 +1,13 @@
 test_that("hasn't changed", {
   expect_known_value(
-    ald_scenario_demo,
-    "ref-ald_scenario_demo",
+    ald_scenario_demo, "ref-ald_scenario_demo",
     update = FALSE
   )
 })
 
 test_that("hasn't changed", {
   expect_known_output(
-    ald_scenario_demo,
-    "ref-ald_scenario_demo-output",
+    ald_scenario_demo, "ref-ald_scenario_demo-output",
     print = TRUE,
     update = FALSE
   )
