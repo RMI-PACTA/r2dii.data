e0b92de1931f2bcceeca85b4a8661ad280bafa23
Mauro Lepore <maurolepore@gmail.com>
2020-05-20 08:25:17 -0500

    Remove @format
    This was something I never saw in other packages and complicated
    maintenance. All datasets are documented in data_dictionary.

--- R/data_dictionary.R
+++ R/data_dictionary.R
@@ -4,14 +4,6 @@
 #'
 #' @family meta
 #'
-#' @format
-#' `data_dictionary` is a [data.frame] with columns:
-#' * `column` (character): The name of a dataset-column.
-#' * `dataset` (character): The name of a dataset.
-#' * `definition` (character): The definition of a dataset-column.
-#' * `typeof` (character): The result of `typeof()`, one of double, integer,
-#' logical, or character.
-#'
 #' @examples
 #' head(data_dictionary)
 "data_dictionary"
--- R/scenario_demo_2020.R
+++ R/scenario_demo_2020.R
@@ -11,22 +11,6 @@
 #' @family demo datasets
 #' @seealso [data_dictionary]
 #'
-#' @format
-#' `scenario_demo_2020` is a [data.frame] with columns:
-#' * `region` (character): The region to which the pathway is relevant.
-#' * `scenario` (character): The name of the scenario.
-#' * `scenario_source` (character): The source publication from which the
-#' scenario was taken.
-#' * `sector` (character): The sector to which the scenario prescribes a
-#' pathway.
-#' * `smsp` (double): Sector market share percentage of the pathway calculated
-#' in 2020.
-#' * `technology` (character): The technology within the sector to which the
-#' scenario prescribes a pathway.
-#' * `tmsr` (double): Technology market share ratio of the pathway calculated in
-#' 2020.
-#' * `year` (integer): The year at which the pathway value is prescribed.
-#'
 #' @examples
 #' head(scenario_demo_2020)
 "scenario_demo_2020"
