ac9fed54f7b074563658722a788833ef32f924db
Mauro Lepore <maurolepore@gmail.com>
2020-05-20 10:25:08 -0500

    Remove spec in loanbook_demo

--- data-raw/loanbook_demo.R
+++ data-raw/loanbook_demo.R
@@ -1,8 +1,7 @@
 source(here::here("data-raw/utils.R"))
 
 # Source: https://github.com/2DegreesInvesting/r2dii.dataraw/pull/4
-
 path <- here::here("data-raw/loanbook_demo.csv")
-loanbook_demo <- readr::read_csv(path)
+loanbook_demo <- remove_spec(readr::read_csv(path))
 
 usethis::use_data(loanbook_demo, overwrite = TRUE)
