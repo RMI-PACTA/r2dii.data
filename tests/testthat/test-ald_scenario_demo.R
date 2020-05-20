744e95f79f14786bdef719735265661e5df3e8a9
Mauro Lepore <maurolepore@gmail.com>
2020-05-20 15:08:37 -0500

    Style

--- tests/testthat/test-ald_scenario_demo.R
+++ tests/testthat/test-ald_scenario_demo.R
@@ -1,4 +1,7 @@
 test_that("hasn't changed", {
   x <- rbind(head(ald_scenario_demo, 250), tail(ald_scenario_demo, 250))
-  expect_known_value(x, "ref-ald_scenario_demo", update = FALSE)
+  expect_known_value(
+    x, "ref-ald_scenario_demo",
+    update = FALSE
+  )
 })
