cbb5a79f77e214ac2c8356cf868290a3b637ed23
Mauro Lepore <maurolepore@gmail.com>
2020-05-20 12:40:57 -0500

    Install devtools and self

--- data-raw/ald_demo.R
+++ data-raw/ald_demo.R
@@ -1,9 +1,13 @@
+library(here)
+library(dplyr)
+
 source(here::here("data-raw/utils.R"))
 
 path <- here::here("data-raw/ald_demo.csv")
 ald_demo <- remove_spec(readr::read_csv(path))
 
-ald_demo <- ald_demo %>%
-  dplyr::left_join(new_emission_factor_unit(), by = "sector")
+ald_demo <- dplyr::left_join(
+  ald_demo, new_emission_factor_unit(), by = "sector"
+)
 
 usethis::use_data(ald_demo, overwrite = TRUE)
