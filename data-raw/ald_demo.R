202acf5cbf88b5e931042157faf1664b22f67faa
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-05-20 10:50:28 +0200

    update ald_demo emission_factor values

--- data-raw/ald_demo.R
+++ data-raw/ald_demo.R
@@ -4,6 +4,6 @@ path <- here::here("data-raw/ald_demo.csv")
 ald_demo <- remove_spec(readr::read_csv(path))
 
 ald_demo <- ald_demo %>%
-  dplyr::left_join(new_emission_factor_unit(), by = "technology")
+  dplyr::left_join(new_emission_factor_unit(), by = "sector")
 
 usethis::use_data(ald_demo, overwrite = TRUE)
