1ceed6f20c318decec6747f6d22f96e31f3656ee
Mauro Lepore <maurolepore@gmail.com>
2020-05-20 13:23:48 -0500

    Pick 500 random rows from ald_scenario_demo

--- tests/testthat/test-ald_scenario_demo.R
+++ tests/testthat/test-ald_scenario_demo.R
@@ -1,6 +1,8 @@
 test_that("hasn't changed", {
+  set.seed(1)
+  rows <- sample(1:nrow(ald_scenario_demo), 500L)
   expect_known_value(
-    ald_scenario_demo, "ref-ald_scenario_demo",
+    ald_scenario_demo[rows, ], "ref-ald_scenario_demo",
     update = FALSE
   )
 })
