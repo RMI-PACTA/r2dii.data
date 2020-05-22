fe099da5d8df3ed19e161d54cc8a6dda2b997a1d
Mauro Lepore <maurolepore@gmail.com>
2020-05-22 04:27:11 -0500

    Remove format (#73)
    This was something I never saw in other packages and complicated
    maintenance. All datasets are documented in data_dictionary.
    
    * Redocument
    
    * Increment version number
    
    * Update NEWS.md
    
    * Closes #63 
    
    Co-authored-by: Jackson Hoffart <jackson.hoffart@gmail.com>

--- R/ald_scenario_demo.R
+++ R/ald_scenario_demo.R
@@ -11,27 +11,6 @@
 #' @family demo datasets
 #' @seealso [data_dictionary]
 #'
-#' @format
-#' `ald_scenario_demo` is a [data.frame] with columns:
-#' * `ald_company_sector_id` (integer): Company ID allocated by 2dii. Each ID is unique per sector and company name.
-#' * `ald_emission_factor` (double): Company level emission factor of the technology.
-#' * `ald_emission_factor_unit` (character): The units that the emission factor is measured in.
-#' * `ald_location` (character): Country where asset is located.
-#' * `ald_production` (double): Company level production of the technology.
-#' * `ald_production_unit` (character): The units that production is measured in.
-#' * `ald_sector` (character): Sector to which the asset belongs.
-#' * `domicile_region` (character): Country where company is registered.
-#' * `id` (character): The financial instrument-specific id that defines the company (or corporate bond pool).
-#' * `id_name` (character): Name of the ID provided in the id column. The id is used for mapping of the dataset to a given portfolio (for credit: company_name for corporate bonds: corporate_ticker for equity: bloomberg_id).
-#' * `is_ultimate_owner` (logical): Flag if company is the ultimate parent in our database.
-#' * `scenario` (character): Abbreviation of the scenario pathway name (e.g. Sustainable Development Scenario is sds).
-#' * `scenario_region` (character): The region of the production (from the asset location). It is aggregated to the regions given by the scenarios to allow the application of the regional scenario pathway.
-#' * `scenario_source` (character): The source publication from which the scenario was taken.
-#' * `smsp` (double): Sector market share percentage.
-#' * `technology` (character): Technology implemented by the asset.
-#' * `tmsr` (double): Technology market share ratio.
-#' * `year` (integer): Year at which the production value is predicted.
-#'
 #' @examples
 #' head(ald_scenario_demo)
 "ald_scenario_demo"
--- R/sector_classifications.R
+++ R/sector_classifications.R
@@ -8,14 +8,6 @@
 #' @family datasets for bridging sector classification codes
 #' @seealso [data_dictionary].
 #'
-#' @format
-#' `sector_classifications` is a [data.frame] with columns:
-#' * `borderline` (character): Flag indicating if 2dii sector and classification
-#' code are a borderline match.
-#' * `code` (character): Formatted code.
-#' * `code_system` (character): Code system.
-#' * `sector` (character): Associated 2dii sector.
-#'
 #' @examples
 #' head(sector_classifications)
 "sector_classifications"
