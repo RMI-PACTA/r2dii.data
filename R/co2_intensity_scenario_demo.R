78b6dbec16824a50c1a616de4c06b4952a3e84b7
Mauro Lepore <maurolepore@gmail.com>
2020-05-26 12:03:32 -0500

    Style helpfile

--- R/co2_intensity_scenario_demo.R
+++ R/co2_intensity_scenario_demo.R
@@ -1,11 +1,11 @@
 #' A prepared co2 intensity climate scenario dataset for demonstration
 #'
 #' @description
-#' Fake co2 intensity climate scenario dataset, prepared for the software PACTA (Paris
-#' Agreement Capital Transition Assessment). It imitates climate scenario data
-#' (e.g. from the International Energy Agency (IEA)) including the change
-#' through time in production across industrial sectors (calculated by
-#' [2DII](https://2degrees-investing.org/)).
+#' Fake co2 intensity climate scenario dataset, prepared for the software
+#' PACTA (Paris Agreement Capital Transition Assessment). It imitates
+#' climate scenario data (e.g. from the International Energy Agency (IEA))
+#' including the change through time in production across industrial sectors
+#' (calculated by [2DII](https://2degrees-investing.org/)).
 #'
 #' @template info_demo-datasets
 #'
