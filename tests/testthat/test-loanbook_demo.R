f050e2a89f72e5e5e15cc5efc4fa7f8edb11b6a7
Mauro Lepore <maurolepore@gmail.com>
2020-06-01 08:46:08 -0500

    Expose one non-ascii characters in loanbook_demo

--- tests/testthat/test-loanbook_demo.R
+++ tests/testthat/test-loanbook_demo.R
@@ -9,3 +9,9 @@ test_that("is not different compared to reference", {
   reference <- readRDS(test_path("ref-loanbook_demo"))
   expect_identical(loanbook_demo, reference)
 })
+
+test_that("has all ascii characters", {
+  x <- r2dii.data::loanbook_demo$name_direct_loantaker
+  is_ascii <- stringr::str_detect(stringi::stri_enc_mark(x), "ASCII")
+  expect_true(all(is_ascii))
+})
