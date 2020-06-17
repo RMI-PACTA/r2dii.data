bbe8ca3943277c1a005c0071823ff2d1c9fa2a29
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-06-17 16:24:55 +0200

    include region isos for etp when sourcing
    FIXME: This is NOT DRY
    Just wanted to get a starting point.

--- data-raw/region_isos.R
+++ data-raw/region_isos.R
@@ -7,7 +7,7 @@ library(usethis)
 # Energy Outlook
 
 this_source <- "weo_2019"
-path <- file.path("data-raw", "region_isos_weo_2019.csv")
+path <- file.path("data-raw", paste0("region_isos_", this_source, ".csv"))
 
 region_data_tibble <- utils::read.csv(
   path,
@@ -91,11 +91,87 @@ if (nrow(fix) > 0L) {
   )
 }
 
-region_isos <- out_only_countries %>%
+region_isos_weo_2019 <- out_only_countries %>%
   rbind(global, developing_economies, iea, non_oecd, non_opec) %>%
   dplyr::left_join(r2dii.data::iso_codes, by = c("value" = "country")) %>%
   dplyr::filter(!is.na(country_iso)) %>%
   dplyr::rename(isos = country_iso) %>%
   dplyr::select(region, isos, source)
+
+######################## SAME but for ETP 2017
+# Source:
+# * raw_regions_etp_2017.csv was transcribed from page 780 of the 2017 Energy
+# Technology Perspectives
+
+this_source <- "etp_2017"
+path <- file.path("data-raw", paste0("region_isos_", this_source, ".csv"))
+
+region_data_tibble <- utils::read.csv(
+  path,
+  colClasses = "character",
+  na.strings = c("", "NA"),
+  stringsAsFactors = FALSE
+) %>%
+  tibble::as_tibble()
+
+region_country_name <- region_data_tibble %>%
+  dplyr::filter(type == "country_name") %>%
+  dplyr::select(-type)
+
+# some regions are cyclically defined using other regions in the raw data
+# we need to expand these and join them back in
+leftover_regions <- dplyr::filter(region_data_tibble, type == "region") %>%
+  dplyr::select(-type)
+
+oecd_asia_oceania_expanded_by_country <- leftover_regions %>%
+  dplyr::filter(region == "oecd asia oceania") %>%
+  dplyr::select(region, value) %>%
+  dplyr::left_join(region_country_name, by = c("value" = "region")) %>%
+  dplyr::select(-value) %>%
+  dplyr::rename(value = value.y)
+
+region_country_name <- rbind(region_country_name, oecd_asia_oceania_expanded_by_country)
+
+oecd <- leftover_regions %>%
+  dplyr::filter(region == "oecd") %>%
+  dplyr::select(region, value) %>%
+  dplyr::left_join(region_country_name, by = c("value" = "region")) %>%
+  dplyr::select(-value) %>%
+  dplyr::rename(value = value.y)
+
+out_only_countries <- region_country_name %>%
+  rbind(oecd) %>%
+  dplyr::arrange(region)
+
+all_countries <- out_only_countries %>%
+  dplyr::select(value) %>%
+  unique()
+
+global <- all_countries %>%
+  dplyr::mutate(region = "global", source = this_source)
+
+# check how many countries dont match their isos
+fix <- out_only_countries %>%
+  dplyr::left_join(r2dii.data::iso_codes, by = c("value" = "country")) %>%
+  dplyr::filter(is.na(country_iso))
+
+if (nrow(fix) > 0L) {
+  warning(
+    "`country_iso` is missing in ", nrow(fix), " rows:
+    Likely not all countries will match or have isos, e.g. smaller polynesian
+    islands may be unmatched. Country definitions are not standardized and
+    change. Match only until you are happy.",
+    call. = FALSE
+  )
+}
+
+region_isos_etp_2017 <- out_only_countries %>%
+  rbind(global) %>%
+  dplyr::left_join(r2dii.data::iso_codes, by = c("value" = "country")) %>%
+  dplyr::filter(!is.na(country_iso)) %>%
+  dplyr::rename(isos = country_iso) %>%
+  dplyr::select(region, isos, source)
+
+region_isos <- rbind(region_isos_weo_2019, region_isos_etp_2017)
 
 usethis::use_data(region_isos, overwrite = TRUE)
