d1f7271ffca5caf49987c1a999aab3ef3bd2ff1e
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-06-22 15:46:40 +0200

    test that all isos are unique per region definition

--- tests/testthat/test-region_isos.R
+++ tests/testthat/test-region_isos.R
@@ -9,3 +9,13 @@ test_that("is not different compared to reference", {
   reference <- readRDS(test_path("ref-region_isos"))
   expect_identical(region_isos, reference)
 })
+
+test_that("isos are not duplicated per region, scenario", {
+  out <- region_isos %>%
+    group_by(region, source) %>%
+    mutate(not_duplicated = !duplicated(.data$isos)) %>%
+    summarize(not_duplicated = all(not_duplicated))
+
+  expect_true(all(out$not_duplicated))
+
+})
