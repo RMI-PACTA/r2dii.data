00a748fd5024ef5d0a09edd17490871d0a70700d
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-07-23 09:16:29 +0200

    gains dataset green_or_brown

---
+++ R/green_or_brown.R
@@ -0,0 +1,15 @@
+#' Determine if a technology is green or brown
+#'
+#' @description
+#' This dataset provides a simple lookup table to determine if a technology is
+#' meant to increase (green) or decrease (brown) to align with a future global
+#' warming of less than 2 degrees.
+#'
+#' @section Definitions:
+#' `r define("green_or_brown")`
+#'
+#' @seealso [data_dictionary]
+#'
+#' @examples
+#' head(green_or_brown)
+"green_or_brown"
