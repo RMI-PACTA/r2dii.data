db9a8377a3e3c6bbda761aee6d9cf68661e7f2fc
Mauro Lepore <maurolepore@gmail.com>
2020-08-12 03:45:21 -0500

    Replace ton and tons with tonne and tonnes (#134)

--- data-raw/utils.R
+++ data-raw/utils.R
@@ -20,7 +20,7 @@ check_no_spec <- function(data) {
 }
 
 co2_per <- function(x) {
-  paste("tons of CO2 per", x)
+  paste("tonnes of CO2 per", x)
 }
 
 new_emission_factor_unit <- function() {
@@ -29,9 +29,9 @@ new_emission_factor_unit <- function() {
     ~sector,     ~ald_emission_factor_unit,
     "automotive",      co2_per("km per car produced"),
     "oil and gas",     co2_per("GJ"),
-    "coal",            co2_per("ton of coal"),
-    "steel",           co2_per("ton of steel"),
-    "cement",          co2_per("ton of cement"),
+    "coal",            co2_per("tonne of coal"),
+    "steel",           co2_per("tonne of steel"),
+    "cement",          co2_per("tonne of cement"),
     "aviation",        co2_per("passenger per km travelled"),
     "power",           co2_per("per hour per MW")
 
