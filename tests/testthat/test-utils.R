961f971b722ab026def3d746c473fbfee2626e7c
Mauro Lepore <maurolepore@gmail.com>
2020-09-10 08:43:30 -0500

    Fix call to  on.exit() to work with R 3.4

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -3,15 +3,16 @@ test_that("source_data_raw() sources .R files and only .R files", {
   if (!dir.exists(data_raw)) {
     dir.create(data_raw)
   }
-  on.exit(unlink(data_raw, recursive = TRUE), add = TRUE, after = FALSE)
+
+  on.exit(unlink(data_raw, recursive = TRUE), add = TRUE)
 
   test_r <- file.path(data_raw, "test.R")
   writeLines("r_test <- 'should exist'", test_r)
-  on.exit(unlink(test_r), add = TRUE, after = FALSE)
+  on.exit(unlink(test_r), add = TRUE)
 
   test_txt <- file.path(data_raw, "test.txt")
   writeLines("txt_test <- 'should not exist'", test_txt)
-  on.exit(unlink(test_txt), add = TRUE, after = FALSE)
+  on.exit(unlink(test_txt), add = TRUE)
 
   source_data_raw(data_raw)
   expect_true(exists("r_test"))
