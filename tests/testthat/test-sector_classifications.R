b25f73ddfd73f12d83a7dfaec9357248fc8dbe47
Mauro Lepore <maurolepore@gmail.com>
2020-11-12 10:14:57 -0800

    Remove ref-sector_classifications

--- tests/testthat/test-sector_classifications.R
+++ tests/testthat/test-sector_classifications.R
@@ -2,11 +2,6 @@ test_that("hasn't change", {
   expect_snapshot_value(sector_classifications, style = "json2")
 })
 
-test_that("is not different compared to reference", {
-  reference <- readRDS(test_path("ref-sector_classifications"))
-  expect_identical(sector_classifications, reference)
-})
-
 test_that("has an entry in data_dictionary", {
   dd <- data_dictionary
   expect_true(nrow(dd[dd$dataset == "sector_classifications", ]) > 0)
