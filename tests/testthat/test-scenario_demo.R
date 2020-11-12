eed6163033ec812bf3fe147b38a6fa4ed2fdec51
Mauro Lepore <maurolepore@gmail.com>
2020-11-12 10:20:22 -0800

    scenario_demo_2020: Replace ref with snapshot

--- tests/testthat/test-scenario_demo.R
+++ tests/testthat/test-scenario_demo.R
@@ -1,11 +1,3 @@
 test_that("hasn't changed", {
-  expect_known_value(
-    scenario_demo_2020, "ref-scenario_demo_2020",
-    update = FALSE
-  )
-})
-
-test_that("is not different compared to reference", {
-  reference <- readRDS(test_path("ref-scenario_demo_2020"))
-  expect_identical(scenario_demo_2020, reference)
+  expect_snapshot_value(scenario_demo_2020, style = "serialize")
 })
