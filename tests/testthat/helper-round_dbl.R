582f1087e990cb8add8eaf98402702c4d4f873cc
Mauro Lepore <maurolepore@gmail.com>
2020-11-13 09:02:44 -0700

    Style

--- tests/testthat/helper-round_dbl.R
+++ tests/testthat/helper-round_dbl.R
@@ -2,6 +2,7 @@ round_dbl <- function(data, digits = 4L) {
   data[detect_dbl(data)] <- lapply(data[detect_dbl(data)], round, digits = digits)
   data
 }
+
 detect_dbl <- function(data) {
   unlist(lapply(data, is.double))
 }
