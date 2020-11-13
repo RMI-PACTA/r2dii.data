2b5b0b486849c6144f196fa603b6f5f6d835b897
Mauro Lepore <maurolepore@gmail.com>
2020-11-13 05:38:05 -0800

    region_isos_demo: Replace ref with snap

--- tests/testthat/test-region_isos_demo.R
+++ tests/testthat/test-region_isos_demo.R
@@ -1,11 +1,3 @@
-test_that("hasn't changed", {
-  expect_known_value(
-    region_isos_demo, "ref-region_isos_demo",
-    update = FALSE
-  )
-})
-
-test_that("is not different compared to reference", {
-  reference <- readRDS(test_path("ref-region_isos_demo"))
-  expect_identical(region_isos_demo, reference)
+test_that("hasn't change", {
+  expect_snapshot_value(region_isos_demo, style = "json2")
 })
