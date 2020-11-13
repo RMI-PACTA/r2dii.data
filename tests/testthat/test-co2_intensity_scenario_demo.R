8e41370a7365e2d90eee99a1f1e6f7db6b5c0687
Mauro Lepore <maurolepore@gmail.com>
2020-11-13 07:52:43 -0700

    co2_intensity_scenario_demo: replace ref with snap

--- tests/testthat/test-co2_intensity_scenario_demo.R
+++ tests/testthat/test-co2_intensity_scenario_demo.R
@@ -1,11 +1,3 @@
-test_that("hasn't changed", {
-  expect_known_value(
-    co2_intensity_scenario_demo, "ref-co2_intensity_scenario_demo",
-    update = FALSE
-  )
-})
-
-test_that("is not different compared to reference", {
-  reference <- readRDS(test_path("ref-co2_intensity_scenario_demo"))
-  expect_identical(co2_intensity_scenario_demo, reference)
+test_that("hasn't change", {
+  expect_snapshot_value(co2_intensity_scenario_demo, style = "serialize")
 })
