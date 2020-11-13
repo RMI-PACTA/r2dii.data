e8db542fcc8abc69d969c8ebb7b54ec982c1cdf0
Mauro Lepore <maurolepore@gmail.com>
2020-11-13 08:49:13 -0700

    Use json2 style

--- tests/testthat/test-scenario_demo_2020.R
+++ tests/testthat/test-scenario_demo_2020.R
@@ -1,3 +1,4 @@
 test_that("hasn't changed", {
-  expect_snapshot_value(scenario_demo_2020, style = "serialize")
+  value <- round_dbl(scenario_demo_2020, 5L)
+  expect_snapshot_value(value, style = "json2")
 })
