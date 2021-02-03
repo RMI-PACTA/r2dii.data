c1d991cb2f84877950f8c99952b6a3e608ac60a8
Mauro Lepore <maurolepore@gmail.com>
2021-02-03 17:05:33 -0600

    Use here in data-raw/classification_bridge.R

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -1,39 +1,35 @@
 library(usethis)
+library(here)
 
-source(file.path("data-raw", "utils.R"))
+source(here("data-raw", "utils.R"))
 
 # Source: @jdhoffa https://github.com/2DegreesInvesting/r2dii.dataraw/pull/6
 isic_classification <- read_csv_(
-  file.path("data-raw", "isic_classification.csv")
+  here("data-raw", "isic_classification.csv")
 )
 use_data(isic_classification, overwrite = TRUE)
 
 nace_classification <- read_csv_(
-  file.path("data-raw", "nace_classification.csv")
+  here("data-raw", "nace_classification.csv")
 )
 use_data(nace_classification, overwrite = TRUE)
 
 naics_classification <- read_csv_(
-  file.path("data-raw", "naics_classification.csv")
+  here("data-raw", "naics_classification.csv")
 )
 usethis::use_data(naics_classification, overwrite = TRUE)
 
 sic_classification <- read_csv_(
-  file.path("data-raw", "sic_classification.csv")
+  here("data-raw", "sic_classification.csv")
 )
 usethis::use_data(sic_classification, overwrite = TRUE)
 
 gics_classification <- read_csv_(
-  file.path("data-raw", "gics_classification.csv")
+  here("data-raw", "gics_classification.csv")
 )
 usethis::use_data(gics_classification, overwrite = TRUE)
 
 cnb_classification <- read_csv_(
-  file.path("data-raw", "cnb_classification.csv")
+  here("data-raw", "cnb_classification.csv")
 )
 usethis::use_data(cnb_classification, overwrite = TRUE)
-
-psic_classification <- read_csv_(
-  file.path('data-raw', 'psic_classification.csv')
-)
-usethis::use_data(psic_classification, overwrite = TRUE)
