7199b298fd49c8646238d99ec179d23b5915353c
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-02-08 16:17:52 +0100

    Add hdv technology identifications to `green_or_brown` (#198)
    Co-authored-by: Mauro Lepore <maurolepore@gmail.com>

--- tests/testthat/helper-round_dbl.R
+++ tests/testthat/helper-round_dbl.R
@@ -3,6 +3,11 @@ round_dbl <- function(data, digits = 4L) {
   data
 }
 
+ceiling_dbl <- function(data) {
+  data[detect_dbl(data)] <- lapply(data[detect_dbl(data)], ceiling)
+  data
+}
+
 detect_dbl <- function(data) {
   unlist(lapply(data, is.double))
 }
