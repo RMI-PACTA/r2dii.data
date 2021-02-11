8bc96b02fb3b19f900d95a04857917310aaae360
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-02-11 10:03:24 +0100

    add failing test

--- tests/testthat/test-sector_classifications.R
+++ tests/testthat/test-sector_classifications.R
@@ -6,3 +6,19 @@ test_that("has an entry in data_dictionary", {
   dd <- data_dictionary
   expect_true(nrow(dd[dd$dataset == "sector_classifications", ]) > 0)
 })
+
+test_that("nace automotive sales are borderline", {
+
+  automotive_sales <- c(
+    451,
+    4511,
+    4519
+  )
+
+  nace_sc <- sector_classifications[sector_classifications$code_system == "NACE", ]
+
+  automotive_sales <- nace_sc[nace_sc$code %in% automotive_sales, ]
+
+  expect_true(all(automotive_sales$borderline))
+
+})
