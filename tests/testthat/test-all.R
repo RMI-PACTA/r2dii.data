c4ace40fb46f42493cbd3edcd7432bd8ac7a1e89
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-02-15 11:03:47 +0100

    test that no dataframe is grouped

--- tests/testthat/test-all.R
+++ tests/testthat/test-all.R
@@ -8,3 +8,14 @@ test_that("no data inherits spec_tbl_df", {
 
   expect_false(any(has_spec))
 })
+
+test_that("no data is grouped", {
+  library(r2dii.data)
+  is_grouped <- vapply(
+    enlist_datasets("r2dii.data"),
+    function(.x) inherits(.x, "grouped_df"),
+    logical(1)
+  )
+
+  expect_false(any(is_grouped))
+})
