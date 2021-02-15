e34366d6ee62c4ef405022dd1c921e2d4d7ac2ac
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-02-15 10:46:40 +0100

    add failing test

--- tests/testthat/test-ald_demo.R
+++ tests/testthat/test-ald_demo.R
@@ -1,3 +1,9 @@
 test_that("hasn't changed", {
   expect_snapshot_value(ceiling_dbl(ald_demo), style = "json2")
 })
+
+test_that("ald_demo has column `id_company`", {
+  ald_demo_columns <- names(ald_demo)
+
+  expect_true("id_company" %in% ald_demo_columns)
+})
