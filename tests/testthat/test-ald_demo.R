b0f60820a7adb2f1175df6827526895eacf3b196
Mauro Lepore <maurolepore@gmail.com>
2021-02-15 09:01:49 -0600

    FIXME? Do we really want a character?

--- tests/testthat/test-ald_demo.R
+++ tests/testthat/test-ald_demo.R
@@ -5,3 +5,7 @@ test_that("hasn't changed", {
 test_that("ald_demo has column `id_company`", {
   expect_true(hasName(ald_demo, "id_company"))
 })
+
+test_that("ald_demo$id_company is of type character, as promised", {
+  expect_type(ald_demo$id_company, "character")
+})
