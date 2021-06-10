d7ef28d2544e896012d14497f4c321ff3adf3b6f
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-06-10 16:15:29 +0200

    add tests and update snapshot

--- tests/testthat/test-ald_demo.R
+++ tests/testthat/test-ald_demo.R
@@ -11,3 +11,12 @@ test_that("ald_demo has column `id_company`", {
 test_that("ald_demo$id_company is of type character, as promised", {
   expect_type(ald_demo$id_company, "character")
 })
+
+test_that("ald_demo$id_company is unique to `name_company` and `sector`", {
+  out <- ald_demo %>%
+    dplyr::select(id_company, name_company, sector) %>%
+    unique() %>%
+    dplyr::filter(duplicated(id_company))
+
+  expect_length(out$id_company, 0L)
+})
