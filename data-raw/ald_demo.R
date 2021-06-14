91fc2ba5e1aff7b2649a0dd2ecc275d62cdc95ab
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-06-14 15:25:09 +0200

    add LEIs to ald_demo

--- data-raw/ald_demo.R
+++ data-raw/ald_demo.R
@@ -3,6 +3,36 @@ library(usethis)
 
 source(file.path("data-raw", "utils.R"))
 
+generate_lei <- function(id) {
+  # function to generate random but reproducible LEIs
+  # 4 characters, 2 zeroes, 12 characters, 2 check digits
+  set.seed(id)
+
+  alpha_num <- c(0:9, LETTERS)
+
+  four <- do.call(
+    paste0,
+    replicate(4, sample(0:9, 1, TRUE), FALSE)
+  )
+
+  twelve <- do.call(
+    paste0,
+    replicate(12, sample(alpha_num, 1, TRUE), FALSE)
+  )
+
+  two <- do.call(
+    paste0,
+    replicate(2, sample(0:9, 1, TRUE), FALSE)
+  )
+
+  paste0(
+    four,
+    "00",
+    twelve,
+    two
+  )
+}
+
 path <- file.path("data-raw", "ald_demo.csv")
 ald_demo <- read_csv_(path)
 
@@ -20,5 +50,38 @@ ald_demo <- ald_demo %>%
     .before = 1
   ) %>%
   ungroup()
+
+leis <- ald_demo %>%
+  # assume only LEIs for ultimate_parents
+  filter(is_ultimate_owner == TRUE) %>%
+  # assume LEIs are unique by id_company
+  select(id_company) %>%
+  unique() %>%
+  # assume LEIs for about 50% of companies
+  sample_n(round(0.5 * nrow(.))) %>%
+  mutate(lei_company = generate_lei(id_company))
+
+ald_demo <- ald_demo %>%
+  left_join(leis, by = "id_company")
+
+ordered_names <- c(
+  "id_company",
+  "name_company",
+  "lei_company",
+  "sector",
+  "technology",
+  "production_unit",
+  "year",
+  "production",
+  "emission_factor",
+  "country_of_domicile",
+  "plant_location",
+  "is_ultimate_owner",
+  "is_ultimate_listed_owner",
+  "ald_timestamp",
+  "ald_emission_factor_unit"
+  )
+
+ald_demo <- select(ald_demo, ordered_names)
 
 usethis::use_data(ald_demo, overwrite = TRUE)
