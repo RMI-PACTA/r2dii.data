9f9299298bc7de1f52e45dacf8d74d2eb1cd82c0
Jackson Hoffart <jackson.hoffart@gmail.com>
2021-07-05 11:14:01 +0200

    add failing test

--- tests/testthat/test-region_isos.R
+++ tests/testthat/test-region_isos.R
@@ -5,3 +5,20 @@ test_that("hasn't changed", {
 test_that("isos are not duplicated per region, scenario", {
   expect_false(any(duplicated(region_isos)))
 })
+
+test_that("outputs regions for expected scenario sources", {
+
+  expected_sources <- c(
+    "weo_2019",
+    "etp_2017",
+    "weo_2020",
+    "isf_2020",
+    "nze_2021"
+  )
+
+  expect_equal(
+    sort(unique(region_isos$source)),
+    sort(expected_sources)
+
+  )
+})
