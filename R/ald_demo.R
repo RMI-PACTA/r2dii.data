8cb61ec58ff88e2da7c99a9ebbbd342a9fd397cb
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-04-26 12:02:40 +0200

    document abcd_demo

--- R/ald_demo.R
+++ R/ald_demo.R
@@ -1,11 +1,14 @@
 #' An asset level dataset for demonstration
 #'
 #' @description
+#' `r lifecycle::badge("superseded")`
 #' Fake data about physical assets (e.g. wind turbine power plant
 #' capacities) used to assess the climate alignment of financial portfolios.
 #' It imitates data from market-intelligence databases.
 #'
 #' @template info_demo-datasets
+#'
+#' @keywords internal
 #'
 #' @section Definitions:
 #' `r define("ald_demo")`
