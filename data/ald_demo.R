e7e5fc17030cd6ba5463d873707be77eccfcf071
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-04-28 17:47:48 +0200

    write warning to mimic lifecycle

--- data/ald_demo.R
+++ data/ald_demo.R
@@ -2,8 +2,8 @@ delayedAssign("ald_demo", {
   on_r_cmd <- !identical(Sys.getenv("R_CMD"), "")
   if (!on_r_cmd) {
     warning(
-      "`ald_demo` will be superseded in r2dii.data 0.3.0 in July 2022.",
-      "Please use `abcd_demo` instead.",
+      "The dataset `ald_demo`is superseded as of r2dii.data 0.3.0 (expected July 2022).",
+      "\nPlease use `abcd_demo` instead.",
       "\nRead this blog post to learn more:",
       "\nhttps://2degreesinvesting.github.io/posts/2022-03-02-ald-becomes-abcd/",
       call. = FALSE
