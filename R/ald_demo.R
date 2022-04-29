a9a8d5522652572674c08f6190b125969cbfc8a5
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-04-29 09:12:44 +0200

    minimize documentation of ald_demo

--- R/ald_demo.R
+++ R/ald_demo.R
@@ -3,19 +3,7 @@
 #' @description
 #' `r lifecycle::badge("superseded")`
 #'
-#' Fake data about physical assets (e.g. wind turbine power plant
-#' capacities) used to assess the climate alignment of financial portfolios.
-#' It imitates data from market-intelligence databases.
-#'
-#' @template info_demo-datasets
+#' See `?abcd_demo`
 #'
 #' @keywords internal
-#'
-#' @section Definitions:
-#' `r define("ald_demo")`
-#'
-#' @seealso [data_dictionary]
-#'
-#' @examples
-#' head(ald_demo)
 "ald_demo"
