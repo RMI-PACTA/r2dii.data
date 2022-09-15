21955500ec58d6b8257bd21adc8dd1181104e512
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-09-15 14:28:07 +0300

    update refs in function documentation

--- R/co2_intensity_scenario_demo.R
+++ R/co2_intensity_scenario_demo.R
@@ -4,8 +4,7 @@
 #' Fake co2 intensity climate scenario dataset, prepared for the software
 #' PACTA (Paris Agreement Capital Transition Assessment). It imitates
 #' climate scenario data (e.g. from the International Energy Agency (IEA))
-#' including the change through time in production across industrial sectors
-#' (calculated by [2DII](https://2degrees-investing.org/)).
+#' including the change through time in production across industrial sectors.
 #'
 #' @template info_demo-datasets
 #'
