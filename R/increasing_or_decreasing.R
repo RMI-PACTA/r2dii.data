9e67fd6b5f0659d5c1df3b982919c7ba5b77c813
Jackson Hoffart <jackson.hoffart@gmail.com>
2023-06-05 10:48:03 +0200

    document dataset

---
+++ R/increasing_or_decreasing.R
@@ -0,0 +1,16 @@
+#' Determine if a technology is increasing or decreasing
+#'
+#' @description
+#' This dataset provides a simple lookup table to determine if a technology is
+#' meant to increase or decrease to align with a scenario that predicts a less
+#' than 2 degree temperature rise.
+#'
+#' @section Definitions:
+#' `r define("increasing_or_decreasing")`
+#'
+#' @family miscellaneous datasets
+#' @seealso [data_dictionary]
+#'
+#' @examples
+#' head(increasing_or_decreasing)
+"increasing_or_decreasing"
