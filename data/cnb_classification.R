d1585f434867db3f49c400b33d6c71188e4f6ddd
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-08 15:11:28 +0100

    use Q2 in deprecation warning

--- data/cnb_classification.R
+++ data/cnb_classification.R
@@ -2,7 +2,7 @@ delayedAssign("cnb_classification", {
   on_r_cmd <- !identical(Sys.getenv("R_CMD"), "")
   if (!on_r_cmd) {
     warning(
-      "The dataset `cnb_classification` will be superseded as of r2dii.data 0.5.0 (expected March 2024).",
+      "The dataset `cnb_classification` will be superseded as of r2dii.data 0.5.0 (expected Q2 2024).",
       "\nPlease see `sector_classifications` for available datasets",
       call. = FALSE
     )
