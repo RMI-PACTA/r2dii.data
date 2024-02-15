880ddf6116695ea85c97239f73290b49715bb2f2
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-02-15 11:38:55 +0100

    add failing test

--- tests/testthat/test-loanbook_demo.R
+++ tests/testthat/test-loanbook_demo.R
@@ -1,3 +1,27 @@
 test_that("hasn't changed", {
   expect_snapshot_value(loanbook_demo, style = "json2")
 })
+
+test_that("loanbook_demo has at least one valid sector classification (#336)", {
+  #base R left_join
+  out <- merge(
+    loanbook_demo,
+    sector_classifications,
+    by.x = c(
+      "sector_classification_system",
+      "sector_classification_direct_loantaker"
+    ),
+    by.y = c(
+      "code_system",
+      "code"
+    ),
+    all.x = TRUE
+  )
+
+  number_of_sector_matches <- nrow(
+    out[!is.na(out[["sector"]]), ]
+  )
+
+  expect_true(number_of_sector_matches > 0)
+
+})
