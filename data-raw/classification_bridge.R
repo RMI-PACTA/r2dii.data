487f28a73fdf8a20ea54ee94ae32907448b11032
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-03-06 14:52:49 +0100

    address jacobs comments

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -50,17 +50,30 @@ isic_classification <- dplyr::mutate(
   sector = dplyr::case_when(
     grepl("^B05", code) ~ "coal",
     grepl("^B06", code) ~ "oil and gas",
-    grepl("^B091", code) ~ "oil and gas",
+    grepl("^B091", code) ~ "oil and gas", # borderline
+    grepl("^B099", code) ~ "coal", # borderline
     grepl("^C2394", code) ~ "cement",
+    grepl("^C2395", code) ~ "cement", # borderline
     grepl("^C241", code) ~ "steel",
-    grepl("^C291", code) ~ "automotive",
-    grepl("^D351", code) ~ "power",
+    grepl("^C2431", code) ~"steel", # borderline
+    grepl("^C291", code) ~ "automotive", # borderline
+    grepl("^C292", code) ~ "automotive", # borderline
+    grepl("^C293", code) ~ "automotive", # borderline
+    grepl("^D351", code) ~ "power", # some of these are borderline
     grepl("^H50", code) ~ "shipping",
     grepl("^H51", code) ~ "aviation",
     TRUE ~ "not in scope"
     ),
   borderline = dplyr::case_when(
     grepl("^B091", code) ~ TRUE,
+    grepl("^B099", code) ~ TRUE,
+    grepl("^C2395", code) ~ TRUE,
+    grepl("^C2431", code) ~ TRUE,
+    grepl("^C291", code) ~ TRUE,
+    grepl("^C292", code) ~ TRUE,
+    grepl("^C293", code) ~ TRUE,
+    code == "D351" ~ TRUE,
+    grepl("^D3513", code) ~ TRUE,
     TRUE ~ FALSE
   ),
 )
