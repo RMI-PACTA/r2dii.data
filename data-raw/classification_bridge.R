db74828b9c070259748b3132461da7d28ea5627c
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-03-11 17:54:50 +0100

    Update data-raw/classification_bridge.R
    Co-authored-by: Jacob Kastl <60064070+jacobvjk@users.noreply.github.com>

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -40,7 +40,7 @@ nace_classification <- dplyr::mutate(
     grepl("^C29.2", code) ~ "automotive", # borderline
     grepl("^C29.3", code) ~ "automotive", # borderline
     grepl("^D35.1", code) ~ "power", # some of these are borderline
-    grepl("^H5.0", code) ~ "shipping",
+    grepl("^H50", code) ~ "shipping",
     grepl("^H51", code) ~ "aviation",
     TRUE ~ "not in scope"
   ),
