38e1bace2b2419530cf49624cb3b3888d2bf7f9b
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-03-19 15:42:46 +0100

    fix borderline = TRUE, not in scope

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -114,7 +114,7 @@ naics_classification <- dplyr::mutate(
     grepl("^32733", code) ~ TRUE,
     grepl("^32739", code) ~ TRUE,
     code == "3273" ~ TRUE,
-   grepl("^21311", code) ~ TRUE,
+    code %in% c("213111", "213112", "213113") ~ TRUE,
     code == "2211" ~ TRUE,
     code %in% c("331512", "331513") ~ TRUE,
     TRUE ~ FALSE
