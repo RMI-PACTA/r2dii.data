01719f0640fb804a920142b01220018a08106354
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-03-19 15:35:06 +0100

    Update data-raw/classification_bridge.R
    Co-authored-by: Jacob Kastl <60064070+jacobvjk@users.noreply.github.com>

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -111,7 +111,7 @@ naics_classification <- dplyr::mutate(
     grepl("^32732", code) ~ TRUE,
     grepl("^32733", code) ~ TRUE,
     grepl("^32739", code) ~ TRUE,
-    code == "213113" ~ TRUE,
+   grepl("^21311", code) ~ TRUE,
     TRUE ~ FALSE
   ),
 )
