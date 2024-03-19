bcadf96544c0223a58c3a06d3c9e4c0d02995bd2
Jackson Hoffart <jackson.hoffart@gmail.com>
2024-03-19 15:38:01 +0100

    Update data-raw/classification_bridge.R
    Co-authored-by: Jacob Kastl <60064070+jacobvjk@users.noreply.github.com>

--- data-raw/classification_bridge.R
+++ data-raw/classification_bridge.R
@@ -102,7 +102,8 @@ naics_classification <- dplyr::mutate(
     grepl("^22111", code) ~ "power",
     code == "2211" ~ "power",
     grepl("^483", code) ~ "shipping",
-    code %in% c("331110", "331512", "331513") ~ "steel",
+    grepl("^3311", code) ~ "steel",
+    code %in% c("331512", "331513") ~ "steel",
     TRUE ~ "not in scope"
   ),
   borderline = dplyr::case_when(
